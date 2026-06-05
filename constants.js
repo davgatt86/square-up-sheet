67fa4bf6df08eb198f3804eff324d839dbdcef9a
davgatt86 <davgatt86@gmail.com>
2026-06-05 22:25:44 +0100

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,27 @@
+export const C = {
+  bg: '#0b1a2c',
+  bgDeep: '#08131f',
+  panel: '#16293d',
+  panel2: '#1d3550',
+  line: '#2a4c6b',
+  ink: '#e8f1f8',
+  dim: '#8badc7',
+  brass: '#d9a441',
+  brassDk: '#a8780f',
+  sea: '#4ab5ae',
+  red: '#d96459',
+  green: '#52b788',
+  amber: '#e9b54a',
+};
+
+export const SHARE_OPTIONS = [
+  { key: 'full', short: 'Full', display: 'Full share' },
+  { key: '7_8', short: '7/8', display: '⅞ (7/8)' },
+  { key: '6_8', short: '6/8', display: '¾ (6/8)' },
+  { key: '5_8', short: '5/8', display: '⅝ (5/8)' },
+  { key: '4_8', short: '4/8', display: '½ (4/8)' },
+  { key: 'custom', short: 'Custom', display: 'Custom…' },
+];
+
+export const SHARE_VAL = { full: 1, '7_8': 0.875, '6_8': 0.75, '5_8': 0.625, '4_8': 0.5 };
+export const QUOTA_OPTS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
